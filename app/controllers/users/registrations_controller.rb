bd3fd2a2e3547c5a6e94b009678a3034b2426a4f
Uthaeus <romanlavery@gmail.com>
2023-06-18 15:23:48 -0600

    created methods for signing up, in, and out in registrations and sessions controllers, added routes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,50 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!, only: [:update, :destroy]
+  respond_to :json
+
+  protected 
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+  end
+
+  private 
+
+  def respond_with(resource, _opts = {})
+    if request.method === "POST" && resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Signed up sucessfully.'},
+        data: resource
+      }, status: :ok
+    elsif request.method === "PUT" && resource.persisted? 
+      @user = User.find_by(id: current_user.id)
+      @user.update({username: resource.username, email: resource.email, password: resource.password, password_confirmation: resource.password_confirmation})
+      render json: {
+        status: {code: 200, message: 'Updated sucessfully.'},
+        data: resource
+      }, status: :ok
+    elsif request.method === "DELETE"
+      @user = User.find_by(id: current_user.id)
+      @user.destroy
+      render json: {
+        status: {code: 200, message: 'Deleted sucessfully.'},
+        data: @user
+      }, status: :ok
+    else
+      render json: {
+        status: {message: "User could not be created or updated"},
+        data: resource.errors
+      }, status: :unprocessable_entity
+    end
+  end
 
   # GET /resource/sign_up
   # def new
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,37 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
+
+  private 
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Logged in sucessfully.'},
+        data: current_user
+      }, status: :ok
+    else
+      render json: {
+        status: {message: "User could not be logged in"},
+        data: resource.errors
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: {
+        status: {code: 200, message: 'Logged out sucessfully.'}
+      }, status: :ok
+    else
+      render json: {
+        status: {message: "User could not be logged out"}
+      }, status: :unprocessable_entity
+    end
+  end
 
   # GET /resource/sign_in
   # def new
